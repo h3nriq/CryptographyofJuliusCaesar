faf775fe97c01d95d0b436ddb77ad9a96a87894c
analuciabolico <analuciabolico.dev@gmail.com>
2019-06-27 10:21:32 -0300

    Add project #1

---
+++ src/main/java/dev/codenation/aceleradevjava2/AceleradevJava2Application.java
@@ -0,0 +1,13 @@
+package dev.codenation.aceleradevjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleradevJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleradevJava2Application.class, args);
+	}
+
+}
